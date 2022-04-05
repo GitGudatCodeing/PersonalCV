ce400edc8f94d10a8af3abef9a79fef84cdabc61
Jason <jddeklerk0@gmail.com>
2022-04-05 16:50:00 +0200

    deploy on gh-pages

--- plugins/vue-tour.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import VueTour from 'vue-tour'
-// You could use your own fancy-schmancy custom styles.
-// We'll use the defaults here because we're lazy.
-import 'vue-tour/dist/vue-tour.css'
-
-Vue.use(VueTour)
